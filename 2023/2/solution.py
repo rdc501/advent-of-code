cb41fdac7525a339368c7701a42125e1c7dcf2f6
rdc501 <rdc501@gmail.com>
2023-12-02 08:12:56 +0000

    Day 2 part 2

--- 2023/2/solution.py
+++ 2023/2/solution.py
@@ -32,6 +32,23 @@ def is_game_possible(game, max_red, max_green, max_blue):
 
     return len(impossible_rounds) == 0
 
+def fewest_cubes_required(game):
+    Cubes = namedtuple('Cubes', 'red green blue')
+    min_red = 0
+    min_green = 0
+    min_blue = 0
+
+    for round in game:
+        if round.red > min_red:
+            min_red = round.red
+        if round.green > min_green:
+            min_green = round.green
+        if round.blue > min_blue:
+            min_blue = round.blue
+
+    return Cubes(min_red, min_green, min_blue)
+
+
 
 file = open("input.txt", "r")
 file_lines = file.readlines()
@@ -43,4 +60,7 @@ for file_line in file_lines:
 
 possible_game_indexes = [ind + 1 for ind, game in enumerate(games) if is_game_possible(game, 12, 13, 14)]
 
-print(sum(possible_game_indexes))
+fewest_cubes = [fewest_cubes_required(game) for game in games]
+fewest_cubes_powers = [cubes.red * cubes.green * cubes.blue for cubes in fewest_cubes]
+
+print(sum(fewest_cubes_powers))

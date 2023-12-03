6e29a262788b107f21378b4ff3fdada4fb7b935e
rdc501 <rdc501@gmail.com>
2023-12-03 10:17:59 +0000

    Day 3 part 1

---
+++ 2023/3/test_solution.py
@@ -0,0 +1,41 @@
+import pytest
+from solution import get_part_numbers, sum_part_numbers
+
+
+@pytest.mark.parametrize(
+    "previous_row, current_row, next_row, expected", [("", "", "", []),
+                                                      ("", "!1", "", ["1"]),
+                                                      ("", "1!", "", ["1"]),
+                                                      ("", "!1..2*", "", ["1", "2"]),
+                                                      ("", "!123..", "", ["123"]),
+                                                      ("", "!123..56$", "", ["123", "56"]),
+                                                      ("", "123..", "", []),
+                                                      ("$....", "123..", "", ["123"]),
+                                                      ("", "123..", "...*.", ["123"]),
+                                                      ("", "123..", "....&", []),
+                                                      ("", "...123", "!....", [])]
+)
+def test_get_part_numbers(previous_row, current_row, next_row, expected):
+    assert get_part_numbers(previous_row, current_row, next_row) == expected
+
+
+def test_sum_part_numbers():
+    test_rows = ["467..114..",
+                 "...*......",
+                 "..35..633.",
+                 "......#...",
+                 "617*......",
+                 ".....+.58.",
+                 "..592.....",
+                 "......755.",
+                 "...$.*....",
+                 ".664.598.."]
+
+    assert sum_part_numbers(test_rows) == 4361
+
+def test_sum_part_numbers_592():
+    test_rows = [".....+.58.",
+                 "..592.....",
+                 "......755."]
+
+    assert sum_part_numbers(test_rows) == 592

382ea4e2e41f58081b02aa678134615b70d44bbd
rdc501 <rdc501@gmail.com>
2023-12-07 08:41:28 +0000

    Day 7 task 1

---
+++ 2023/7/solution.py
@@ -0,0 +1,106 @@
+def get_hand_types(hands):
+    hand_types = {
+        'five': [],
+        'four': [],
+        'full_house': [],
+        'three': [],
+        'two_pair': [],
+        'one_pair': [],
+        'highest_card': []
+    }
+
+    for hand_string in hands:
+        hand = Hand(hand_string)
+
+        if hand.highest_number_of_cards() == 5:
+            hand_types['five'].append(hand)
+            continue
+        if hand.highest_number_of_cards() == 4:
+            hand_types['four'].append(hand)
+            continue
+        if hand.highest_number_of_cards() == 3 and hand.second_highest_number_of_cards() == 2:
+            hand_types['full_house'].append(hand)
+            continue
+        if hand.highest_number_of_cards() == 3:
+            hand_types['three'].append(hand)
+            continue
+        if hand.highest_number_of_cards() == 2 and hand.second_highest_number_of_cards() == 2:
+            hand_types['two_pair'].append(hand)
+            continue
+        if hand.highest_number_of_cards() == 2:
+            hand_types['one_pair'].append(hand)
+            continue
+
+        hand_types['highest_card'].append(hand)
+
+    def sort_by_first_card(hand):
+        return hand.cards_value()
+
+    for key, value in hand_types.items():
+        value.sort(key=sort_by_first_card)
+
+    return hand_types
+
+def calculate_winnings(hand_types):
+    ranking = 1
+    total = 0
+    hand_lists = list(hand_types.items())
+    hand_lists.reverse()
+
+    for hand_list in hand_lists:
+        for hand in hand_list[1]:
+            total += ranking * int(hand.original[6:])
+            ranking += 1
+
+    return total
+
+class Hand():
+    def __init__(self, input):
+        self.original = input
+
+        self.card_counts = {}
+        cards = input.split(' ')[0]
+        self.bid = int(input.split(' ')[1])
+
+        for card in cards:
+            self.card_counts[card] = self.card_counts.get(card, 0) + 1
+
+    def highest_number_of_cards(self):
+        # key = max(self.card_counts, key=self.card_counts.get)
+        # return self.card_counts[key]
+        def fu(e):
+            return e[1]
+
+        z = list(self.card_counts.items())
+        z.sort(key=fu, reverse=True)
+        return z[0][1]
+
+    def second_highest_number_of_cards(self):
+        # key = max(self.card_counts, key=self.card_counts.get)
+        # return self.card_counts[key]
+        def fu(e):
+            return e[1]
+
+        z = list(self.card_counts.items())
+        z.sort(key=fu, reverse=True)
+        return z[1][1]
+
+    def cards_value(self):
+        card_types = ['2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A']
+        # return card_types.index(self.original[0])
+        result = ''
+        for card in self.original[:5]:
+            result += str(card_types.index(card)).zfill(2)
+        return int(result)
+
+
+
+
+
+
+file = open("input.txt", "r")
+file_lines = file.readlines()
+
+hand_types = get_hand_types(file_lines)
+winnings = calculate_winnings(hand_types)
+print(winnings)

5f3f91b07146623077dc19244d5339cbda5ea539
britgamer <britgamer@hotmail.com>
2023-12-10 22:33:51 +0000

    Day 10 attempt at task 2. Works for test data but does not find any enclosed tiles on real input

--- 2023/10/solution.py
+++ 2023/10/solution.py
@@ -56,25 +56,156 @@ def find_start_pipe(rows):
 
         row_index += 1
 
+def get_route(rows, start_pipe):
+    route = []
+    number_of_moves = 1
+    first_pipe = start_pipe[0] + 1, start_pipe[1] # Bit of a cheat from looking at the input
+    # first_pipe = start_pipe[0], start_pipe[1] + 1 # Bit of a cheat from looking at the input
+    location = first_pipe
+    entry = (-1, 0)
+    # entry = (0, -1)
+    current_pipe = rows[location[1]][location[0]]
+
+    route.append((start_pipe[1], start_pipe[0]))
+
+    while current_pipe != "S":
+        route.append((location[1], location[0]))
+        move = next_pipe(current_pipe, entry)
+        location = location[0] + move[0], location[1] + move[1]
+
+        current_pipe = rows[location[1]][location[0]]
+        entry = 0 - move[0], 0 - move[1]
+        number_of_moves += 1
+
+    return route
+
+
+def get_potential_enclosed_tiles(rows, route):
+    tiles = []
+    row_index = 0
+    while row_index < len(rows):
+        column_index = 0
+        new_row = ""
+        while column_index < len(rows[row_index]):
+            if (row_index, column_index) in route:
+                new_row += rows[row_index][column_index]
+            else:
+                new_row += "?"
+            column_index += 1
+        tiles.append(new_row)
+        row_index += 1
+
+    return tiles
+
+
+def exclude_tiles_connected_to_edge(rows):
+    tiles = []
+    row_index = 0
+    change_made = False
+    while row_index < len(rows):
+        column_index = 0
+        new_row = ""
+        while column_index < len(rows[row_index]):
+            if rows[row_index][column_index] == "?" or rows[row_index][column_index] == "o":
+                if row_index == 0 or column_index == 0:
+                    new_row += "."
+                    change_made = True
+                elif row_index == len(rows) - 1 or column_index == len(rows[row_index]) - 1:
+                    new_row += "."
+                    change_made = True
+                elif tiles[row_index - 1][column_index] == ".":
+                    new_row += "."
+                    change_made = True
+                elif new_row[-1] == ".":
+                    new_row += "."
+                    change_made = True
+                else:
+                    new_row += rows[row_index][column_index]
+            else:
+                new_row += rows[row_index][column_index]
+            column_index += 1
+        row_index += 1
+        tiles.append(new_row)
+    return (tiles, change_made)
+
+
+def flip_reverse_rows(rows):
+    return list(reversed([row[::-1] for row in rows]))
+
+
+def expand_rows(rows):
+    new_rows = ["o".join(list(row)) for row in rows]
+
+    row_index = 0
+    while row_index < len(new_rows):
+        column_index = 1
+        while column_index < len(new_rows[row_index]):
+            if new_rows[row_index][column_index] == "S":
+                new_rows[row_index] = new_rows[row_index][:column_index] + "-" + new_rows[row_index][column_index + 1:]
+            if new_rows[row_index][column_index] == "o":
+                if new_rows[row_index][column_index - 1] == ".":
+                    new_rows[row_index] = new_rows[row_index][:column_index] + "." + new_rows[row_index][column_index + 1:]
+                elif new_rows[row_index][column_index - 1] == "-" or \
+                   new_rows[row_index][column_index - 1] == "F" or \
+                   new_rows[row_index][column_index - 1] == "L":
+                    new_rows[row_index] = new_rows[row_index][:column_index] + "-" + new_rows[row_index][column_index + 1:]
+            column_index += 1
+        row_index += 1
+
+    result = []
+    for row in new_rows:
+        result.append(row)
+        new_row = []
+        index = 0
+        while index < len(row):
+            match row[index]:
+                case ".":
+                    new_row.append(".")
+                case "|":
+                    new_row.append("|")
+                case "F":
+                    new_row.append("|")
+                case "7":
+                    new_row.append("|")
+                case _:
+                    new_row.append("o")
+            index += 1
+        result.append("".join(new_row))
+
+    return result
+
+
+def process_lines(rows):
+    start_pipe = find_start_pipe(rows)
+    route = get_route(rows, start_pipe)
+
+    print(len(route))
+    print(len(route) / 2)
+
+    potential_enclosed_tiles = get_potential_enclosed_tiles(rows, route)
+    # for row in potential_enclosed_tiles:
+    #     print(row)
+    potential_enclosed_tiles = exclude_tiles_connected_to_edge(potential_enclosed_tiles)[0]
+
+    potential_enclosed_tiles = exclude_tiles_connected_to_edge(flip_reverse_rows(potential_enclosed_tiles))[0]
+    potential_enclosed_tiles = flip_reverse_rows(potential_enclosed_tiles)
+
+    expanded_rows = expand_rows(potential_enclosed_tiles)
+    # for row in expanded_rows:
+    #     print(row)
+
+    changes_made = True
+    while changes_made:
+        expanded_rows, changes_made = exclude_tiles_connected_to_edge(flip_reverse_rows(expanded_rows))
+
+    for row in flip_reverse_rows(expanded_rows):
+        print(row)
+
+    print("".join(expanded_rows).count("?"))
+
+
 if __name__ == "__main__":
     file = open("input.txt", "r")
     file_lines = file.readlines()
 
-    start_pipe = find_start_pipe(file_lines)
-
-    number_of_moves = 1
-    first_pipe = start_pipe[0] + 1, start_pipe[1] # Bit of a cheat from looking at the input
-    location = first_pipe
-    entry = (-1, 0)
-    current_pipe = file_lines[location[1]][location[0]]
-
-    while current_pipe != "S":
-        move = next_pipe(current_pipe, entry)
-        location = location[0] + move[0], location[1] + move[1]
-
-        current_pipe = file_lines[location[1]][location[0]]
-        entry = 0 - move[0], 0 - move[1]
-        number_of_moves += 1
-
-    print(number_of_moves)
-    print(number_of_moves / 2)
+    process_lines(file_lines)
--- 2023/10/test_solution.py
+++ 2023/10/test_solution.py
@@ -1,5 +1,5 @@
 import pytest
-from solution import can_move_to_pipe, next_pipe, find_start_pipe
+from solution import can_move_to_pipe, next_pipe, find_start_pipe, process_lines
 
 
 @pytest.mark.parametrize(
@@ -90,7 +90,7 @@ def test_next_pipe(pipe, entry, expected):
     assert next_pipe(pipe, entry) == expected
 
 
-def test_find_start_pipr():
+def test_find_start_pipe():
     rows = ["-L|F7",
             "7S-7|",
             "L|7||",
@@ -98,3 +98,32 @@ def test_find_start_pipr():
             "L|-JF"]
 
     assert find_start_pipe(rows) == (1, 1)
+
+
+def test_process_lines_simple():
+    lines = ["..........",
+             ".S------7.",
+             ".|F----7|.",
+             ".||....||.",
+             ".||....||.",
+             ".|L-7F-J|.",
+             ".|..||..|.",
+             ".L--JL--J.",
+             ".........."]
+
+    process_lines(lines)
+
+
+def test_process_lines_complex():
+    lines = ["FF7FSF7F7F7F7F7F---7",
+             "L|LJ||||||||||||F--J",
+             "FL-7LJLJ||||||LJL-77",
+             "F--JF--7||LJLJ7F7FJ-",
+             "L---JF-JLJ.||-FJLJJ7",
+             "|F|F-JF---7F7-L7L|7|",
+             "|FFJF7L7F-JF7|JL---7",
+             "7-L-JL7||F7|L7F-7F7|",
+             "L.L7LFJ|||||FJL7||LJ",
+             "L7JLJL-JLJLJL--JLJ.L"]
+
+    process_lines(lines)

ed96437555c5159f9c6d5a33663b6ff51cd515db
rdc501 <rdc501@gmail.com>
2024-12-06 12:57:31 +0000

    Day one

---
+++ 2024/DayOnePuzzleOne.cs
@@ -0,0 +1,37 @@
+namespace _2024;
+
+public static class DayOnePuzzleOne
+{
+    public static string Execute()
+    {
+        var (firstList, secondList) = GetSortedLists();
+        
+        var result = firstList.Select((t, i) => Math.Abs(t - secondList[i])).Sum();
+
+        return result.ToString();
+    }
+    
+    public static (List<int>, List<int>) GetSortedLists()
+    {
+        var lines = File.ReadAllLines("inputs/day_one_puzzle_one.txt");
+
+        var firstList = new List<int>();
+        var secondList = new List<int>();
+
+        foreach (var line in lines)
+        {
+            var items = line.Split(" ")
+                .Where(x => !string.IsNullOrWhiteSpace(x))
+                .Select(int.Parse)
+                .ToList();
+            
+            firstList.Add(items[0]);
+            secondList.Add(items[1]);
+        }
+        
+        firstList.Sort();
+        secondList.Sort();
+
+        return (firstList, secondList);
+    }
+}
---
+++ 2024/DayOnePuzzleTwo.cs
@@ -0,0 +1,70 @@
+namespace _2024;
+
+public class DayOnePuzzleTwo
+{
+    public static string Execute()
+    {
+        var (firstList, secondList) = DayOnePuzzleOne.GetSortedLists();
+
+        var result = 0;
+        
+        var cache = new Dictionary<int, int>();
+        
+        foreach (var item in firstList)
+        {
+            if (cache.TryGetValue(item, out var value))
+                result += item * value;
+            
+            var firstInstanceIndex = GetFirstInstanceIndex(item, secondList);
+
+            if (firstInstanceIndex is not null)
+            {
+                secondList = secondList[(int)firstInstanceIndex..];
+                
+                var numberOfMatches = NumberOfMatches(item, secondList);
+                
+                cache[item] = numberOfMatches;
+                result += item * numberOfMatches;
+                secondList = secondList[numberOfMatches..];
+            }
+        }
+        
+        return result.ToString();
+    }
+
+    private static int? GetFirstInstanceIndex(int value, List<int> list)
+    {
+        var result = (int?)null;
+
+        for (var i = 0; i < list.Count; i++)
+        {
+            if (list[i] == value)
+            {
+                result = i;
+                break;
+            }
+
+            if (list[i] > value)
+            {
+                break;
+            }
+        }
+        
+        return result;
+    }
+
+    private static int NumberOfMatches(int value, List<int> list)
+    {
+        var result = 0;
+
+        foreach (var t in list)
+        {
+            if (t != value)
+                break;
+
+            result++;
+        }
+        
+        return result;
+    }
+}

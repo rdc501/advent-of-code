1f39fe34b034776dcff9fdeed28ecc976aa1cf63
rdc501 <84025857+rdc501@users.noreply.github.com>
2025-12-01 19:23:27 +0000

    Day 9 task 2

--- 2023/9/solution.py
+++ 2023/9/solution.py
@@ -10,6 +10,20 @@ def get_extrapolated_value(values):
     else:
         return values[-1] + get_extrapolated_value(differences)
 
+def get_pretrapolated_value(values):
+    index = 1
+    differences = []
+    while index - len(values):
+        differences.append(values[index] - values[index - 1])
+        index += 1
+
+    if differences.count(0) == len(differences):
+        return values[0]
+    else:
+        return values[0] - get_pretrapolated_value(differences)
+
+
+
 def convert_line(line):
     values = line.split(' ')
     return [int(value) for value in values]
@@ -21,3 +35,6 @@ if __name__ == "__main__":
     
     extrapolated_values = [get_extrapolated_value(convert_line(file_line)) for file_line in file_lines]
     print(sum(extrapolated_values))
+
+    pretrapolated_value = [get_pretrapolated_value(convert_line(file_line)) for file_line in file_lines]
+    print(sum(pretrapolated_value))
--- 2023/9/test_solution.py
+++ 2023/9/test_solution.py
@@ -1,5 +1,5 @@
 import pytest
-from solution import get_extrapolated_value
+from solution import get_extrapolated_value, get_pretrapolated_value
 
 @pytest.mark.parametrize(
     "input, expected", [([0, 0, 0, 0, 0], 0),
@@ -8,3 +8,14 @@ from solution import get_extrapolated_value
 )
 def test_get_extrapolated_value(input, expected):
     assert get_extrapolated_value(input) == expected
+
+
+@pytest.mark.parametrize(
+    "input, expected", [([0, 0, 0, 0, 0], 0),
+                        ([2, 2, 2], 2),
+                        ([0, 2, 4, 6], -2),
+                        ([3, 3, 5, 9, 15], 5),
+                        ([10, 13, 16, 21, 30, 45], 5)]
+)
+def test_get_pretrapolated_value(input, expected):
+    assert get_pretrapolated_value(input) == expected

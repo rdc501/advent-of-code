1cba45640b0c684c8750a21c870cc77dcb9af1a4
rdc501 <84025857+rdc501@users.noreply.github.com>
2025-12-01 19:23:27 +0000

    Day one

---
+++ 2024/Program.cs
@@ -0,0 +1,33 @@
+﻿using Spectre.Console;
+
+namespace _2024;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        while (true) {
+            var selectedTask = AnsiConsole.Prompt(
+                new SelectionPrompt<string>()
+                    .Title("[green]What puzzle do you want to run?[/]")
+                    .PageSize(10)
+                    .MoreChoicesText("[grey](Move up and down to reveal more)[/]")
+                    .AddChoices(new[] {
+                        "day one puzzle one",
+                        "day one puzzle two",
+                    }));
+            
+            AnsiConsole.MarkupLine($"[green]Selected:[/] {selectedTask}");
+            
+             var result = selectedTask switch
+            {
+                "day one puzzle one" => DayOnePuzzleOne.Execute(),
+                "day one puzzle two" => DayOnePuzzleTwo.Execute(),
+                _ => throw new NotImplementedException()
+            };
+            
+            AnsiConsole.MarkupLine($"[green]Result:[/] {result}");
+            Console.ReadLine();
+        }
+    }
+}

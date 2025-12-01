c6ebafe5155792d8aa1ce6ed28dedc021a1b8d29
rdc501 <84025857+rdc501@users.noreply.github.com>
2025-12-01 19:23:27 +0000

    Day 4 task 2

--- 2023/4/solution.py
+++ 2023/4/solution.py
@@ -1,3 +1,6 @@
+from collections import namedtuple
+
+
 def parse_row(row):
     row = row.split(':')[1]
     sections = row.split('|')
@@ -21,10 +24,37 @@ def get_score(matching_numbers):
 
     return pow(2, len(matching_numbers) - 1)
 
+def get_cards(all_matching_numbers):
+    Result = namedtuple("Result", "winning_numbers amount")
+    cards = [Result(winning_numbers, 1) for winning_numbers in all_matching_numbers]
+
+    index = 0
+    while index < len(all_matching_numbers):
+        cards_won = len(all_matching_numbers[index])
+        amount_to_add = cards[index].amount
+
+        if cards_won > 0:
+            cards_added = 1
+            while cards_added <= cards_won:
+                card = cards[index + cards_added]
+                cards[index + cards_added] = Result(card.winning_numbers, card.amount + amount_to_add)
+                cards_added += 1
+        index += 1
+
+    return cards
+
+def get_total_cards(cards):
+    amounts = [card.amount for card in cards]
+    return sum(amounts)
+
+
+
 
 file = open("input.txt", "r")
 file_lines = file.readlines()
 
-scores = [get_score(get_matching_numbers(*parse_row(row))) for row in file_lines]
+all_matching_numbers = [get_matching_numbers(*parse_row(row)) for row in file_lines]
+scores = [get_score(matching_numbers) for matching_numbers in all_matching_numbers]
 
 print(sum(scores))
+print(get_total_cards(get_cards(all_matching_numbers)))
--- 2023/4/test_solution.py
+++ 2023/4/test_solution.py
@@ -1,5 +1,5 @@
 import pytest
-from solution import parse_row, get_matching_numbers, get_score
+from solution import parse_row, get_matching_numbers, get_score, get_total_cards, get_cards
 
 def test_parse_row():
     row = 'Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53'
@@ -23,3 +23,15 @@ def test_get_matching_numbers():
 )
 def test_get_score(input, expected):
     assert get_score(input) == expected
+
+
+def test_get_total_cards():
+    rows = ['Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53',
+            'Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19',
+            'Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1',
+            'Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83',
+            'Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36',
+            'Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11']
+
+    all_matching_numbers = [get_matching_numbers(*parse_row(row)) for row in rows]
+    assert get_total_cards(get_cards(all_matching_numbers)) == 30

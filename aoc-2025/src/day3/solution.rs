fb14334711e4872afebf9db66d00941a7a08974b
rdc501 <84025857+rdc501@users.noreply.github.com>
2025-12-06 14:31:45 +0000

    aoc 2025 day 3 puzzle 2

--- aoc-2025/src/day3/solution.rs
+++ aoc-2025/src/day3/solution.rs
@@ -4,7 +4,7 @@ pub fn puzzle_1() -> i64 {
     let mut total_joltage = 0;
 
     for line in contents.lines() {
-        total_joltage += get_max_joltage(line.parse().unwrap());
+        total_joltage += get_max_joltage(line.parse().unwrap(), 2);
     }
 
     println!("Answer: {}", total_joltage);
@@ -12,36 +12,62 @@ pub fn puzzle_1() -> i64 {
     total_joltage
 }
 
-fn get_max_joltage(battery_bank: String) -> i64 {
-    let mut batteries = battery_bank.chars()
-        .rev()
-        .map(|c| c.to_string().parse().expect("oops"));
+pub fn puzzle_2() -> i64 {
+    let contents = include_str!("./input.txt");
 
-    let mut first = 0;
-    let mut second = 0;
-    let mut first_index = 0;
+    let mut total_joltage = 0;
 
-    let mut first_batteries = batteries.clone();
-
-    first_batteries.next();
-
-    for (index, battery) in first_batteries.enumerate() {
-        if battery >= first {
-            // Don't think enumberate() takes into account the next() we did before the loop
-            first_index = index + 1;
-            first = battery;
-        }
+    for line in contents.lines() {
+        total_joltage += get_max_joltage(line.parse().unwrap(), 12);
     }
 
-    for (index, battery) in batteries.enumerate() {
-        if index < first_index {
-            if battery > second {
-                second = battery
+    println!("Answer: {}", total_joltage);
+
+    total_joltage
+}
+
+fn get_max_joltage(battery_bank: String, number_of_batteries: usize) -> i64 {
+    let batteries: Vec<i64> = battery_bank.chars()
+        .rev()
+        .map(|c| c.to_string().parse().expect("oops"))
+        .collect();
+
+    let mut max_index = batteries.len();
+
+    // let number_of_batteries = 2;
+    let mut joltages: Vec<i64> = Vec::new();
+
+    while joltages.len() < number_of_batteries {
+        let mut joltage = 0;
+        let min_index = number_of_batteries - joltages.len() - 1;
+
+        (joltage, max_index) = highest_number(batteries.clone(), min_index, max_index);
+        joltages.push(joltage);
+    }
+
+    let mut max_joltage = 0;
+
+    for joltage in joltages {
+        max_joltage = (max_joltage * 10) + joltage;
+    }
+
+    max_joltage
+}
+
+fn highest_number(numbers: Vec<i64>, min_index: usize, max_index: usize) -> (i64, usize) {
+    let mut result: i64 = 0;
+    let mut result_index = 0;
+
+    for (index, number) in numbers.iter().enumerate() {
+        if index >= min_index && index < max_index {
+            if *number >= result {
+                result = *number;
+                result_index = index;
             }
         }
     }
 
-    (first * 10) + second
+    (result, result_index)
 }
 
 #[cfg(test)]
@@ -50,23 +76,26 @@ mod get_max_joltage_tests {
     use super::*;
 
     #[rstest(::trace)]
-    #[case("987654321111111", 98)]
-    #[case("811111111111119", 89)]
-    #[case("234234234234278", 78)]
-    #[case("818181911112111", 92)]
-    #[case("00", 0)]
-    #[case("12", 12)]
-    #[case("21", 21)]
-    #[case("123", 23)]
-    #[case("213", 23)]
-    #[case("312", 32)]
-    #[case("132", 32)]
-    #[case("231", 31)]
-    #[case("321", 32)]
-
-    #[case("3402", 42)]
-    #[case("4342", 44)]
-    fn result_is_correct(#[case]battery_bank: String, #[case]expected: i64) {
-        assert_eq!(get_max_joltage(battery_bank), expected);
+    #[case("987654321111111", 2, 98)]
+    #[case("811111111111119", 2, 89)]
+    #[case("234234234234278", 2, 78)]
+    #[case("818181911112111", 2, 92)]
+    #[case("00", 2, 0)]
+    #[case("12", 2, 12)]
+    #[case("21", 2, 21)]
+    #[case("123", 2, 23)]
+    #[case("213", 2, 23)]
+    #[case("312", 2, 32)]
+    #[case("132", 2, 32)]
+    #[case("231", 2, 31)]
+    #[case("321", 2, 32)]
+    #[case("3402", 2, 42)]
+    #[case("4342", 2, 44)]
+    #[case("987654321111111", 12, 987654321111)]
+    #[case("811111111111119", 12, 811111111119)]
+    #[case("234234234234278", 12, 434234234278)]
+    #[case("818181911112111", 12, 888911112111)]
+    fn result_is_correct(#[case]battery_bank: String, #[case]number_of_batteries: usize, #[case]expected: i64) {
+        assert_eq!(get_max_joltage(battery_bank, number_of_batteries), expected);
     }
 }
--- aoc-2025/src/main.rs
+++ aoc-2025/src/main.rs
@@ -2,10 +2,6 @@ mod day3 {
     pub mod solution;
 }
 
-// fn main() {
-//     day1::solution::puzzle_1();
-// }
-
 fn main() {
-    day3::solution::puzzle_1();
+    day3::solution::puzzle_2();
 }
